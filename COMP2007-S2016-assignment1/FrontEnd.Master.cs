1a11dfeb7a3b1281e16d11a02b53f1909c1f4a0a
Jasssim <jasim.khan89@gmail.com>
2016-06-01 01:20:45 -0400

    added addActiveClass method in FrontEnd.Master.cs

--- COMP2007-S2016-assignment1/FrontEnd.Master.cs
+++ COMP2007-S2016-assignment1/FrontEnd.Master.cs
@@ -11,7 +11,36 @@ namespace COMP2007_S2016_assignment1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            addActiveClass();
+        }
+        /**
+         * This method adds the active class to all li 
+         * in the main navbar as per the page clicked.
+         * 
+         * @method addActiveClass
+         * @return {string}
+         */
+        private string addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+                case "Contacts":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+            return Page.Title;
         }
     }
 }
